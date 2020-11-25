ac763e007e7e201cf90cf7e82ac3cca6fdca25ce
siddartharyan <68399822+siddartharyan@users.noreply.github.com>
2020-11-25 15:22:05 +0530

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -10,33 +10,33 @@ const states = [
         name: "Indore",
         towns: [
           {
-            name: "Mhow",
+            name: "Mhow"
           },
           {
-            name: "Dewas",
-          },
-        ],
+            name: "Dewas"
+          }
+        ]
       },
       {
         name: "Bhopal",
         towns: [
           {
-            name: "Manit",
+            name: "Manit"
           },
           {
-            name: "Berasia",
-          },
-        ],
+            name: "Berasia"
+          }
+        ]
       },
       {
         name: "Gwalior",
         towns: [
           {
-            name: "Ajaypur",
-          },
-        ],
-      },
-    ],
+            name: "Ajaypur"
+          }
+        ]
+      }
+    ]
   },
   {
     name: "Jharkhand",
@@ -45,36 +45,36 @@ const states = [
         name: "Dhanbad",
         towns: [
           {
-            name: "IIT(ISM) Dhanbad",
+            name: "IIT(ISM) Dhanbad"
           },
           {
-            name: "Hirapur",
-          },
-        ],
+            name: "Hirapur"
+          }
+        ]
       },
       {
         name: "Wasseypur",
         towns: [
           {
-            name: "Sardar khan's",
+            name: "Sardar khan's"
           },
           {
-            name: "Faizal khan's",
-          },
-        ],
+            name: "Faizal khan's"
+          }
+        ]
       },
       {
         name: "Mirzapur",
         towns: [
           {
-            name: "Kaleen bhaiya's",
+            name: "Kaleen bhaiya's"
           },
           {
-            name: "Guddu bhaiya's",
-          },
-        ],
-      },
-    ],
+            name: "Guddu bhaiya's"
+          }
+        ]
+      }
+    ]
   },
   {
     name: "Assam",
@@ -83,36 +83,36 @@ const states = [
         name: "Guwhati",
         towns: [
           {
-            name: "Amin",
+            name: "Amin"
           },
           {
-            name: "Jalah",
-          },
-        ],
+            name: "Jalah"
+          }
+        ]
       },
       {
         name: "Jungle1",
         towns: [
           {
-            name: "Tiger found at IIT Guwahati",
+            name: "Tiger found at IIT Guwahati"
           },
           {
-            name: "Leopard found in IIT Guwahati",
-          },
-        ],
+            name: "Leopard found in IIT Guwahati"
+          }
+        ]
       },
       {
         name: "Tezpur",
         towns: [
           {
-            name: "Dibrugarh",
+            name: "Dibrugarh"
           },
           {
-            name: "Silchar",
-          },
-        ],
-      },
-    ],
+            name: "Silchar"
+          }
+        ]
+      }
+    ]
   },
   {
     name: "Bihar",
@@ -121,41 +121,112 @@ const states = [
         name: "Patna",
         towns: [
           {
-            name: "Sonpur",
+            name: "Sonpur"
           },
           {
-            name: "Maner",
-          },
-        ],
+            name: "Maner"
+          }
+        ]
       },
       {
         name: "Gaya",
         towns: [
           {
-            name: "Bakraur",
+            name: "Bakraur"
           },
           {
-            name: "Barachatti",
-          },
-        ],
+            name: "Barachatti"
+          }
+        ]
       },
       {
         name: "Darbhanga",
         towns: [
           {
-            name: "Singhwara",
+            name: "Singhwara"
           },
           {
-            name: "Jale",
-          },
-        ],
-      },
-    ],
-  },
+            name: "Jale"
+          }
+        ]
+      }
+    ]
+  }
 ];
 
 function App() {
-  return <div id="main"></div>;
+  let [cities, setCities] = useState([]);
+  let [towns, setTowns] = useState([]);
+  const handleCity = (index) => {
+    let obj = undefined;
+    for (let i = 0; i < states.length; i++) {
+      if (i === index) {
+        obj = states[i];
+        break;
+      }
+    }
+    let cities1 = obj["cities"];
+    obj = [];
+    for (let i = 0; i < cities1.length; i++) {
+      obj.push(cities1[i]["name"]);
+    }
+    setCities(obj);
+  };
+
+  const handleTown = (city) => {
+    let obj = undefined;
+    let found = false;
+    for (let i = 0; i < states.length; i++) {
+      let arr = states[i]["cities"];
+      console.log(arr);
+      for (let j = 0; j < arr.length; j++) {
+        if (arr[j]["name"] === city) {
+          obj = arr[j]["towns"];
+          found = true;
+          break;
+        }
+      }
+      if (found) {
+        break;
+      }
+    }
+    console.log(obj);
+    let arr = [];
+    for (let i = 0; i < obj.length; i++) {
+      arr.push(obj[i]["name"]);
+    }
+    setTowns(arr);
+  };
+  return (
+    <div id="main">
+      <h1>states</h1>
+      <div>
+        {states.map((place, index) => {
+          return (
+            <div id={`state${index + 1}`} onClick={() => handleCity(index)}>
+              {place["name"]}
+            </div>
+          );
+        })}
+      </div>
+      <h1>cities</h1>
+      <div>
+        {cities.map((place, index) => {
+          return (
+            <div id={`city${index + 1}`} onClick={() => handleTown(place)}>
+              {place}
+            </div>
+          );
+        })}
+      </div>
+      <h1>towns</h1>
+      <div>
+        {towns.map((place, index) => {
+          return <div id={`town${index + 1}`}>{place}</div>;
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default App;
